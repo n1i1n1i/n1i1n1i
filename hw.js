da61963e76c02ce0d9479db72f5f046736212577
n1i1n1i <ninipukhashvili91@gmail.com>
2023-11-07 18:48:43 +0400

    add homework

---
+++ hw.js
@@ -0,0 +1,53 @@
+//1 
+let x=87; let y=65;
+let c=(x+y);
+console.log(c);
+//2
+let arr =["iphones","samsung","pixel","huawei","xiaomi"];
+console.log(arr);
+//3
+arr[4]="Oneplus";
+console.log(arr);
+//4
+
+let student = {
+    firstname : "Nini",
+    lastname : "Pukhashvili",
+    age : 32,
+    email : "ninipukhashvili91@gmail.com",
+    marks : 80
+}
+console.log(student);
+//5
+
+
+console.log(student.firstname);
+//6
+
+let m=45;
+let n=23;
+let sum = m+n;
+if (m>n){console.log(sum);}
+else {console.log(n);}
+//7
+
+let studentname ="nini";
+console.log("Rise and shine,"+ studentname +"!");
+//8 
+
+let salary =3000;
+let bonus;
+if (salary>=2000) {bonus=0;}
+else {bonus=salary*0.10;}
+console.log(bonus)
+//9
+
+ a=23;
+ b=a/2;
+ c= (a+b);
+console.log(c)
+//10
+
+let str1= "ნინი ფუხაშვილი";
+let str2= "სამშაბათი";
+console.log("მოგესალმებით"+ str1+"და"+"დღეს"+str2);
